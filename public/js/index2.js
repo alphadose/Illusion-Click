879fae38de848503efee017fc222c0bffc20b6bc
alphadose <anish.mukherjee1996@gmail.com>
2017-03-29 17:06:55 +0530

    Final

--- public/js/index2.js
+++ public/js/index2.js
@@ -10,10 +10,10 @@
     function initHeader() {
         width = window.innerWidth;
         height = window.innerHeight;
-        target = {x: width/2, y: height/2};
+        target = {x: width, y: height};
 
         largeHeader = document.getElementById('large-header');
-        largeHeader.style.height = 1366+'px';
+        largeHeader.style.height = height+'px';
 
         canvas = document.getElementById('demo-canvas');
         canvas.width = width;
