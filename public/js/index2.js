5377b3fccceab6926081ed12bdc44c3ccacdd937
alphadose <anish.mukherjee1996@gmail.com>
2017-03-29 18:01:19 +0530

    zzz

--- public/js/index2.js
+++ public/js/index2.js
@@ -12,7 +12,7 @@
         height = window.innerHeight;
         target = {x: width/2, y: height/2}
         largeHeader = document.getElementById('large-header');
-        largeHeader.style.height = 1920+'px';
+        largeHeader.style.height = height+'px';
         
         canvas = document.getElementById('demo-canvas');
         canvas.width = width;
