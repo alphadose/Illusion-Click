b1a01bb9950e25bc7c8611ff15d50ba0b2e8c8ce
alphadose <anish.mukherjee1996@gmail.com>
2017-03-29 17:29:38 +0530

    Final

--- public/js/index2.js
+++ public/js/index2.js
@@ -17,7 +17,7 @@
         
         canvas = document.getElementById('demo-canvas');
         canvas.width = width;
-        canvas.height = 1366;
+        canvas.height = 1920;
         ctx = canvas.getContext('2d');
 
         // create points
