97b271b4508b9b3b55b8a94e7e989f809ee3f34e
alphadose <anish.mukherjee1996@gmail.com>
2017-03-29 15:10:55 +0530

    Final

--- app/models/SignUp.php
+++ app/models/SignUp.php
@@ -12,7 +12,7 @@
 		public static function getDB()
 		{
 			include __DIR__."/../../configs/credentials.php" ;
-			return new \PDO("mysql:dbname=".$db_connect['db_name'].";host=".$db_connect['server'] , $db_connect['username'] , $db_connect['password']) ;
+			return new \PDO("pgsql:dbname=".$db_connect['db_name'].";host=".$db_connect['server'] , $db_connect['username'] , $db_connect['password']) ;
 		}
 
 		public static function checkUser($username , $score)
--- configs/credentials.php
+++ configs/credentials.php
@@ -1,8 +1,8 @@
 <?php
 $db_connect = [
-    "db_name"=>"IC_GAME",
-    "server"=>"localhost",
-	"username"=>"root",
-	"password"=>"alphakimori"
+    "db_name"=>"d5aouostsepgig",
+    "server"=>"ec2-54-235-248-197.compute-1.amazonaws.com",
+	"username"=>"sgezvcaczjgwzh",
+	"password"=>"1d13c3a1e865659ae27350b037946b74ca617bc46dbfb75b2b0ae0c98830a89f"
 ]
 ?>
