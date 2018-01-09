4698d838cf416620ccf48379389259f83a02f7d8
Alphadose <anish.mukherjee1996@gmail.com>
2018-01-09 17:16:26 +0530

    updated configs

--- configs/credentials.php
+++ configs/credentials.php
@@ -1,8 +1,8 @@
 <?php
 $db_connect = [
-    "db_name"=>"d5aouostsepgig",
-    "server"=>"ec2-54-235-248-197.compute-1.amazonaws.com",
-	"username"=>"sgezvcaczjgwzh",
-	"password"=>"1d13c3a1e865659ae27350b037946b74ca617bc46dbfb75b2b0ae0c98830a89f"
+    "db_name"=>getenv('DATABASE_NAME');,
+    "server"=>getenv('SERVER');,
+	"username"=>getenv('USERNAME');,
+	"password"=>getenv('PASSWORD');
 ]
 ?>
---
+++ configs/credentials.sample.php
@@ -0,0 +1,8 @@
+<?php
+$db_connect = [
+    "db_name"=>"";,
+    "server"=>"";,
+	"username"=>"";,
+	"password"=>"";
+]
+?>
